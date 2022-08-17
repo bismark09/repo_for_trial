835127daf499471049d81b644dd548983407739d
yuri.filatov <yuri@filatov.pro>
2022-08-17 15:47:56 +0300

    dev

---
+++ tt_bus_irf.sv
@@ -0,0 +1,12 @@
+
+interface tt_bus_irf(
+	logic 			req,
+	logic [31:0]	addr,
+	logic 			cmd,
+	logic [31:0]	wdata,
+	logic 			ack,
+	logic [31:0]	rdata
+);
+
+
+endinterface
--- tt_tb.sv
+++ tt_tb.sv
@@ -1,6 +1,6 @@
 module tt_tb();
 
-
+/*
 tt_crossbar #(2,2)(
 
 input 			master_req 		[NUM_MASTER-1],
@@ -9,10 +9,8 @@ input 			master_cmd		[NUM_MASTER-1],
 input [31:0]	master_wdata	[NUM_MASTER-1],
 output 			master_ack		[NUM_MASTER-1],
 output [31:0]	master_rdata	[NUM_MASTER-1],
-
-
 );
-
+*/
 
 
 
